65ca3944d2b21b76c5c3ec10d60ec751d4f51a73
hafzanwazani <hafzanwazani03@gmail.com>
2024-11-09 12:30:39 +0800

    Add files via upload

---
+++ prog6 (1).py
@@ -0,0 +1,24 @@
+# variabel assigment value
+x = 9
+y = 6
+print("nilai dari x adalah : ", x)  # Menggunakan x, bukan y
+
+# aturan penamaan variabel
+luas = 30  # bisa berupa kata 
+hafzan = 100  # jika dua kata, digabung dengan "_"
+Keziax = 10  # boleh gabung capital non-kapital
+keziax = 3
+lenca88 = 10  # jika menggabungkan huruf dan angka, angkanya di akhir
+print("nilai dari kezia adalah : ", lenca88)  # Penulisan dari yang benar
+
+a = 4  # integer bilangan bulat
+print("tipe data dari a : ", type(a))  # Penulisan dari yang benar
+
+b = 6.9  # float bilangan pecahan
+print("tipe data dari b : ", type(b))  # Penulisan dari yang benar
+
+c = "Gacor"  # string, gabungan karakter
+print("tipe data dari c : ", type(c))  # Penulisan dari UU yang benar
+
+d = False  # boolean, true false
+print("tipe data dari d : ", type(d))
